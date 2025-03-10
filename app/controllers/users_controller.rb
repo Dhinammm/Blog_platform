6abde29d0e683afee1e67a3b040e02ce69740c1b
Dhinakaran <dhinaharanmohan79@gmail.com>
2025-03-10 19:07:34 +0530

    Revert "Resolving indentation problem"
    This reverts commit f80f2f812cbe9a2668bee9918eff8e35869e2994.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,48 +1,38 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: %i[show destroy]
-
-    def index
-        @users = User.all
+  def index
+    @user = User.all
+  end
+  
+  def show
+    begin
+      @user = User.find(params[:id])
+      @article = Article.where("user_id = ?",params[:id])
+    rescue
+      render file:Rails.public_path.join('404.html'), status: :not_found, layout: true
     end
+  end
 
-    def show
-        @articles = Article.where(user_id: @user.id)
-    rescue ActiveRecord::RecordNotFound
-        not_found_method
+  def new 
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def new
-        @user = User.new
-    end
+  def destroy
+     user = User.find(params[:id])
+     user.destroy
+     redirect_to users_path
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            redirect_to @user
-        else
-            render :new, status: :unprocessable_entity
-        end
-    end
-
-    def destroy
-        @user.destroy
-        redirect_to users_path
-    end
-
-    private
-
-    def set_user
-        @user = User.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-        not_found_method
-    end
-
+  private
     def user_params
-        params.require(:user).permit(:name, :email, :password)
-    end
-
-    def not_found_method
-        render file: Rails.public_path.join("404.html"), status: :not_found, layout: true
+      params.expect(user: [:name, :email, :password])
     end
 end
-
