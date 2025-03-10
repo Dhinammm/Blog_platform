f80f2f812cbe9a2668bee9918eff8e35869e2994
Dhinakaran <dhinaharanmohan79@gmail.com>
2025-03-10 17:48:39 +0530

    Resolving indentation problem

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,60 +1,59 @@
 class ArticlesController < ApplicationController
-  def index
-    @article = Article&.all
-  end
+    before_action :set_article, only: %i[show edit update destroy]
 
-  def show
-    begin
-      @article = Article&.find(params[:id])
-      @user = @article.user_id
-    rescue
-      not_found_method
+    def index
+        @articles = Article.all
     end
-  end
 
-  def new
-    @article = Article.new
-  end
-
-  def create
-    @article = Article.new(article_params)
-    if @article.save
-      redirect_to @article
-    else
-      render :new, status: :unprocessable_entity
+    def show
+        @user = @article.user_id
+    rescue ActiveRecord::RecordNotFound
+        not_found_method
     end
-  end
 
-  def edit
-    begin
-      @article = Article.find(params[:id])
-    rescue
-      not_found_method
+    def new
+        @article = Article.new
     end
-  end
 
-  def update
-    @article = Article.find(params[:id])
-    if @article.update(article_params)
-      redirect_to @article
-    else
-      render :edit, status: :unprocessable_entity
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to @article
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
-  end
 
-  def destroy
-    article = Article.find(params[:id])
-    user = article.user_id
-    article.destroy
-    redirect_to user_path(user)
-  end
-  
-  def not_found_method
-    render file: Rails.public_path.join('404.html'), status: :not_found, layout: true
-  end
+    def edit; end
 
-  private
+    def update
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        user = @article.user_id
+        @article.destroy
+        redirect_to user_path(user)
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        not_found_method
+    end
+
     def article_params
-      params.expect(article: [:title, :content, :user_id])
+        params.require(:article).permit(:title, :content, :user_id)
+    end
+
+    def not_found_method
+        render file: Rails.public_path.join("404.html"), status: :not_found, layout: true
     end
 end
+
